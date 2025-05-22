868fe7d648bc628e8f2555280fce8a80123b3cbd
hawkeyetherapper <hawkeyetherapper@gmail.com>
2025-05-22 23:38:29 +0000

    xb

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CGLE0OZz.mjs';
-import { manifest } from './manifest_BuoOYC5s.mjs';
+import { manifest } from './manifest_DPCSGnAc.mjs';
 
 const serverIslandMap = new Map();;
 
